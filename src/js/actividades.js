aa9d265647dce1469ba0791a55765787a086882f
Camilobarre <barrecami@gmail.com>
2024-04-17 13:44:59 -0500

    Se empiezan los ejercicios para practicar con numeros

---
+++ src/js/actividades.js
@@ -0,0 +1,38 @@
+//Ejercicio 1
+numero1 = 25
+numero2 = 10
+console.info(numero1 + numero2)
+
+//Ejercicio 2
+console.info(numero1 - numero2)
+
+//Ejericicio 3
+console.info(numero1 * numero2)
+
+//Ejericcio 4
+console.info(numero1 / numero2)
+
+//Ejercicio 6
+console.info(numero1 % numero2)
+
+//Ejercicio 7
+let numeroAleatorio = Math.floor(Math.random() * 10) + 1;
+console.info(numeroAleatorio)
+
+//Ejercicio 8
+console.info(Math.sqrt(numero1))
+
+//Ejercicio 9
+let gradoCelsius = 30
+console.info(gradoCelsius*(9/5)+32)
+
+//Ejercicio 10
+let radio=5
+console.info(Math.PI*radio*radio)
+
+//Ejercicio 11
+let lado= 4
+console.info(lado*4)
+
+//Ejercicio 12
+console.info((4/3)*Math.PI*radio*radio)
