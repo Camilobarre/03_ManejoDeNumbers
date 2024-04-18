67d91141727bab777f068fec7a23c5cf3494722b
Camilobarre <barrecami@gmail.com>
2024-04-18 07:42:24 -0500

    Se continua con los ejercicios

--- src/js/actividades.js
+++ src/js/actividades.js
@@ -1,38 +1,486 @@
-//Ejercicio 1
-numero1 = 25
-numero2 = 10
-console.info(numero1 + numero2)
+console.groupCollapsed('Ejercicio 1');
+// Ejercicio 1
+numero1 = 25;
+numero2 = 10;
+console.info(numero1 + numero2);
+console.groupEnd();
 
-//Ejercicio 2
-console.info(numero1 - numero2)
+console.groupCollapsed('Ejercicio 2');
+// Ejercicio 2
+console.info(numero1 - numero2);
+console.groupEnd();
 
-//Ejericicio 3
-console.info(numero1 * numero2)
+console.groupCollapsed('Ejercicio 3');
+// Ejercicio 3
+console.info(numero1 * numero2);
+console.groupEnd();
 
-//Ejericcio 4
-console.info(numero1 / numero2)
+console.groupCollapsed('Ejercicio 4');
+// Ejercicio 4
+console.info(numero1 / numero2);
+console.groupEnd();
 
-//Ejercicio 6
-console.info(numero1 % numero2)
+console.groupCollapsed('Ejercicio 5');
+// Ejercicio 5
+console.info(numero1 % numero2);
+console.groupEnd();
 
-//Ejercicio 7
-let numeroAleatorio = Math.floor(Math.random() * 10) + 1;
-console.info(numeroAleatorio)
+console.groupCollapsed('Ejercicio 6');
+// Ejercicio 6
+let numeroAleatorio = Math.floor(Math.random() * 10 + 1 - 1) + 1;
+console.info(numeroAleatorio);
+console.groupEnd();
 
-//Ejercicio 8
-console.info(Math.sqrt(numero1))
+console.groupCollapsed('Ejercicio 7');
+// Ejercicio 7
+console.info(Math.pow(2, 3));
+console.groupEnd();
 
-//Ejercicio 9
-let gradoCelsius = 30
-console.info(gradoCelsius*(9/5)+32)
+console.groupCollapsed('Ejercicio 8');
+// Ejercicio 8
+console.info(Math.sqrt(numero1));
+console.groupEnd();
 
-//Ejercicio 10
-let radio=5
-console.info(Math.PI*radio*radio)
+console.groupCollapsed('Ejercicio 9');
+// Ejercicio 9
+let gradoCelsius = 30;
+console.info(gradoCelsius * (9 / 5) + 32);
+console.groupEnd();
 
-//Ejercicio 11
-let lado= 4
-console.info(lado*4)
+console.groupCollapsed('Ejercicio 10');
+// Ejercicio 10
+let radio = 5;
+console.info(Math.PI * radio * radio);
+console.groupEnd();
 
-//Ejercicio 12
-console.info((4/3)*Math.PI*radio*radio)
+console.groupCollapsed('Ejercicio 11');
+// Ejercicio 11
+let lado = 4;
+console.info(lado * 4);
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 12');
+// Ejercicio 12
+console.info((4 / 3) * Math.PI * radio * radio);
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 13');
+// Ejercicio 13
+base = 5;
+altura = 4;
+console.info((base * altura) / 2);
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 14');
+// Ejercicio 14
+for (i = 0; i <= 10; i + 1) {
+  resultado = 5 * i;
+  console.info(resultado);
+}
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 15');
+// Ejercicio 15
+console.info(Math.max(3, 7, 2));
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 16');
+// Ejercicio 16
+console.info(Math.min(10, 20, 5));
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 17');
+// Ejercicio 17
+console.info((4 + 6 + 8) / 3);
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 18');
+// Ejercicio 18
+let factorial = 1;
+for (let i = 2; i <= 5; i++) {
+  factorial *= i;
+}
+console.info(factorial);
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 19');
+// Ejercicio 19
+console.info(Math.round(3.6));
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 20');
+// Ejercicio 20
+console.info(9 * 2);
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 21');
+// Ejercicio 21
+console.info(4 * 3);
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 22');
+// Ejercicio 22
+console.info(Math.pow(6, 2));
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 23');
+// Ejercicio 23
+console.info(Math.pow(3, 3));
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 24');
+// Ejercicio 24
+let suma = 0;
+for (let i = 1; i <= 10; i++) {
+  suma += i;
+}
+console.info(suma);
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 25');
+// Ejercicio 25
+console.info(50 - 25);
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 26');
+// Ejercicio 26
+console.info(7 * 9);
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 27');
+// Ejercicio 27
+console.info(144 / 12);
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 28');
+// Ejercicio 28
+console.info(17 % 4);
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 29');
+// Ejercicio 29
+let numeroAleatorio2 = Math.floor(Math.random() * 11 + 20);
+console.info(numeroAleatorio2);
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 30');
+// Ejercicio 30
+console.info(Math.pow(2, 8));
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 31');
+// Ejercicio 31
+console.info(Math.sqrt(144));
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 32');
+// Ejercicio 32
+console.info(100 * (9 / 5) + 32);
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 33');
+// Ejercicio 33
+baserect = 5;
+alturarect = 10;
+console.info(baserect * alturarect);
+console.console.groupEnd();
+
+console.groupCollapsed('Ejercicio 34');
+// Ejercicio 34
+console.info(2 * Math.PI * 6);
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 35');
+// Ejercicio 35
+console.info(4 * 4 * 4);
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 36');
+// Ejercicio 36
+console.info((8 * 10) / 2);
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 37');
+// Ejercicio 37
+console.info(((6 * 10) / 2) * 4);
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 38');
+// Ejercicio 38
+for (i = 0; i <= 10; i + 1) {
+  resultado = 8 * i;
+  console.info(resultado);
+}
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 39');
+// Ejercicio 39
+console.info(Math.max(15, 20, 25));
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 40');
+// Ejercicio 40
+console.info(Math.min(50, 75, 100));
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 41');
+// Ejercicio 41
+console.info((7 + 9 + 12) / 3);
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 42');
+// Ejercicio 42
+let factorial2 = 1;
+for (let i = 2; i <= 7; i++) {
+  factorial2 *= i;
+}
+console.info(factorial2);
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 43');
+// Ejercicio 43
+console.info(Math.round(7.9));
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 44');
+// Ejercicio 44
+console.info(15 * 2);
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 45');
+// Ejercicio 45
+console.info(6 * 3);
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 46');
+// Ejercicio 46
+console.info(Math.pow(8, 2));
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 47');
+// Ejercicio 47
+console.info(Math.pow(4, 3));
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 48');
+// Ejercicio 48
+let suma2 = 0;
+for (let i = 1; i <= 15; i++) {
+  suma2 += i;
+}
+console.info(suma2);
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 49');
+// Ejercicio 49
+console.info(100 - 55);
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 50');
+// Ejercicio 50
+console.info(6 * 8);
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 51');
+// Ejercicio 51
+console.info(225 / 15);
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 52');
+// Ejercicio 52
+console.info(20 % 7);
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 53');
+// Ejercicio 53
+let numeroAleatorio3 = Math.floor(Math.random() * 11 + 40);
+console.info(numeroAleatorio3);
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 54');
+// Ejercicio 54
+console.info(Math.pow(3, 4));
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 55');
+// Ejercicio 55
+console.info(Math.sqrt(169));
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 56');
+// Ejercicio 56
+console.info(80 * (9 / 5) + 32);
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 57');
+// Ejercicio 57
+baserect2 = 8;
+alturarect2 = 12;
+console.info(baserect2 * alturarect2);
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 58');
+// Ejercicio 58
+console.info(2 * Math.PI * 8);
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 59');
+// Ejercicio 59
+console.info(5 * 5 * 5);
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 60');
+// Ejercicio 60
+console.info((12 * 16) / 2);
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 61');
+// Ejercicio 61
+console.info(((8 * 12) / 2) * 6);
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 62');
+// Ejercicio 62
+for (i = 0; i <= 10; i + 1) {
+  resultado = 9 * i;
+  console.info(resultado);
+}
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 63');
+// Ejercicio 63
+console.info(Math.max(25.3, 35));
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 64');
+// Ejercicio 64
+console.info(Math.min(80, 95, 110));
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 65');
+// Ejercicio 65
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 66');
+// Ejercicio 66
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 67');
+// Ejercicio 67
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 68');
+// Ejercicio 68
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 69');
+// Ejercicio 69
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 70');
+// Ejercicio 70
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 71');
+// Ejercicio 71
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 72');
+// Ejercicio 72
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 73');
+// Ejercicio 73
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 74');
+// Ejercicio 74
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 75');
+// Ejercicio 75
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 76');
+// Ejercicio 76
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 77');
+// Ejercicio 77
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 78');
+// Ejercicio 78
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 79');
+// Ejercicio 79
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 80');
+// Ejercicio 80
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 81');
+// Ejercicio 81
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 82');
+// Ejercicio 82
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 83');
+// Ejercicio 83
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 84');
+// Ejercicio 84
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 85');
+// Ejercicio 85
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 86');
+// Ejercicio 86
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 87');
+// Ejercicio 87
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 88');
+// Ejercicio 88
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 89');
+// Ejercicio 89
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 90');
+// Ejercicio 90
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 91');
+// Ejercicio 91
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 92');
+// Ejercicio 92
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 93');
+// Ejercicio 93
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 94');
+// Ejercicio 94
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 95');
+// Ejercicio 95
+console.groupEnd();
+
+console.groupCollapsed('Ejercicio 96');
+// Ejercicio 96
+console.groupEnd();
